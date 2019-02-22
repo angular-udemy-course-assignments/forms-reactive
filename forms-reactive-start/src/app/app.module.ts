b23e8946646636ed726b69c3e5e8b9db0504cba2
nkolchakov <nikola.kolchakov@gmail.com>
2019-02-22 16:36:15 +0200

    creating the form in ts code

--- forms-reactive-start/src/app/app.module.ts
+++ forms-reactive-start/src/app/app.module.ts
@@ -11,7 +11,7 @@ import {ReactiveFormsModule} from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
